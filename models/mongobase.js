74ebc655ff96350c52d55c4989ed3b1ab384bffc
dantegg <luroufan327@hotmail.com>
2016-12-23 13:08:58 +0800

    update

---
+++ models/mongobase.js
@@ -0,0 +1,55 @@
+/**
+ * Created by dantegg on 16-12-23.
+ */
+const {ObjectID} = require('mongodb')
+
+class MongoBaseModel{
+    init(collection){
+        this.collection = collection
+    }
+
+    toId(id){
+        if(id instanceof ObjectID){
+            return id
+        }
+
+        return new ObjectID(id)
+    }
+
+    async create(obj){
+        const insertResult = await this.collection.insertOne(obj)
+        return insertResult && insertResult.insertedId
+    }
+
+    get(id){
+        return await this.collection.findOne({_id:this.toId(id)})
+    }
+
+    update(id,obj){
+        return await this.collection.updateOne({_id:this.toId(id)},obj)
+    }
+
+    updatePart(id,part){
+        return await this.collection.updateOne({_id:this.toId(id)},{$set:part})
+    }
+    del(id){
+        return await this.collection.deleteOne({_id:this.toId(id)})
+    }
+
+    deleteMany(query={}){
+        return this.collection.deleteMany(query)
+    }
+
+    find(query={},sort={},limit=100){
+        return this.collection.find(query).sort(sort).limit(limit)
+    }
+    findBefore(before,limit=100){
+        return this.find({_id:{$lt:this.toId(before)}},{_id:-1})  //降序查询
+    }
+    findSince(since,limit=100){
+        return this.find({_id:{$gt:this.toId(since)}},{_id:-1})     //升序查询
+    }
+
+}
+
+module.exports = MongoBaseModel
--- models/user.js
+++ models/user.js
@@ -1,22 +1,23 @@
 /**
  * Created by dantegg on 16-12-22.
  */
-const  BaseModel = require('./base')
-const PREFIX_EMAIL_TO_ID = 'email-id'
-class UserModel extends BaseModel{
-    constructor(store){
-        super(store,'user:')
+const MongoBaseModel = require('./mongobase')
+
+
+class UserModel extends MongoBaseModel{
+    init(collection){
+        this.collection = collection
+        this.collection.createIndex({email:1},{unique:true}).then()
     }
 
-    create(obj){
-        return super.create(obj).then((id)=>{
-            return this.store.set(PREFIX_EMAIL_TO_ID+obj.email,id).then(()=>id)
-        })
-    }
+    // create(obj){
+    //     return super.create(obj).then((id)=>{
+    //         return this.store.set(PREFIX_EMAIL_TO_ID+obj.email,id).then(()=>id)
+    //     })
+    // }
 
-    async getByEmail(email){
-        const id = await this.store.get(PREFIX_EMAIL_TO_ID+email)
-        return await this.get(id)
+    getByEmail(email){
+        return this.collection.findOne({email:email})
     }
 }
 

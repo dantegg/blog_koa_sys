20a2117f8ae6891fde0dc47b6bceb5f884c793c7
dantegg <luroufan327@hotmail.com>
2017-01-06 16:22:49 +0800

    update

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,17 @@ router.get('/session/get',async(ctx)=>{
     ctx.body = ctx.session
 })
 
+router.get('/session/set',async(ctx)=>{
+    ctx.session.foo = 'bar'
+    ctx.session.time = Date.now()
+    ctx.body = ctx.session
+})
+
+router.get('/session/reset',async(ctx)=>{
+    ctx.session = null
+    ctx.body = 'reseted'
+})
+
 router.post('/test',async(ctx)=>{
     ctx.body = {
         foo:'bar',
